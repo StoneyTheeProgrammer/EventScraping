0d9b707f1d4c6ebfe3fc45ac595c79eb61910b98
StoneyTheeProgrammer <67203608+StoneyTheeProgrammer@users.noreply.github.com>
2020-12-11 21:21:09 -0500

    Create events.py

---
+++ events.py
@@ -0,0 +1,8 @@
+# Used 'mbasic' to strip away the CSS, this block logs into the bare facebook page without style
+# With this code, we would just need to change the URL
+
+eventList = ["https://www.facebook.com/events/236916930979252",
+"https://www.facebook.com/events/425032691289553",
+"https://www.facebook.com/events/2989522954452548",
+"https://www.facebook.com/events/3771105592941602",
+"https://www.facebook.com/events/591810158438628"]
